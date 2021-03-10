0af8382ce40778a5c4410be842ec727c578e1b5c
Kevin Jiménez <ji.oso.kev@gmail.com>
2021-03-10 16:43:49 -0600

    Conexion exitosa con la API y creacion de cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-const hola = "Hola";
+import './styles/main.css';
 
-console.log(hola);
+const id = '1cd38fdfb4e576db86733fbddac42215';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
+const mountNode = document.getElementById("jsNode");
+const City = document.getElementById("city");
+const button = document.getElementById("search");
+
+
+const createCard = async () => {
+    let searchCity = City.value;
+    const response = await fetch(`${baseUrl}q=${searchCity}&appid=${id}`);
+    const data = await response.json();
+
+    const nameCity = document.createElement("h3");
+        nameCity.className = "weather-card-title";
+        nameCity.textContent = data.name;
+
+        const temperatura = document.createElement("h2");
+        temperatura.className = "weather-card-temp";
+        temperatura.textContent = data.main.temp;
+
+        const image = document.createElement("img");
+        image.className = "weather-image";
+        image.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+        const pronostico = document.createElement("p");
+        pronostico.className = "weather-card-pron";
+        pronostico.textContent = data.weather.[0].description;
+
+        const card = document.createElement("article");
+        card.className = "weather-detail-card";
+        card.appendChild(nameCity);
+        card.appendChild(temperatura);
+        card.appendChild(image);
+        card.appendChild(pronostico);
+
+        mountNode.append(card);
+        City.value = "";
+};
+
+button.addEventListener('click', createCard);
